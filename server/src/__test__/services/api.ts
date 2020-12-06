8c1c0eaeb9e6161513e8b6cda881b72da0cdd953
TGF0911 <gennari0911@gmail.com>
2020-12-06 00:08:05 -0300

    Add UpdateDoctors Page

--- server/src/__test__/services/api.ts
+++ server/src/__test__/services/api.ts
@@ -1,8 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3434',
-});
+  baseURL: 'http://localhost:3434'
+})
 
-export default api;
-
+export default api
